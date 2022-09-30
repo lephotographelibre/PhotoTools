64daafb4ae06269d44e3e0ac18c6ec211dd88470
lephotographelibre <lephotographelibre@yahoo.com>
2022-09-30 14:17:57 +0200

    1st Commit - Repo creation

---
+++ README.md
@@ -0,0 +1,17 @@
+# What?
+PhotoTools is a collection of Python >= 3.8 programs to manipulate images and photographies
+
+# How ?
+Pre-requisites
+
+Python >= 3.8
+
+### Requirements
+
+ - [Pillow](https://python-pillow.org/) (see [installation instructions](https://pillow.readthedocs.io/en/stable/installation.html))
+
+install it with `pip install Pillow` or with package manager from your distro
+
+### List of Tools
+
+- **slicer** can be used to slice (vertically or horizontally) an image/photo. Parameters (orientation=H/V, number of slices=nbslices)
---
+++ slicer.py
@@ -0,0 +1,107 @@
+import ntpath
+from PIL import Image
+
+# TODO transform s=V/H slicer in crop box slicer (x * y)
+
+# Input Parameters
+max_pixel = 2000
+nb_slices = 3
+# TODO crop = "3x5"
+slice_orientation = "V"
+input_image = "images/vertical01.jpg"
+
+# Prepare Data - Variables
+print("Input file = " + input_image)
+# extract filename from imput file name
+filename = ntpath.basename(input_image)
+index = filename.index('.')
+filename = filename[:index]
+# print(file_name)
+output_image = "out/" + filename + "_slice"
+print("Output file = " + output_image)
+
+if slice_orientation == "V":
+    print("Slice orientation = " + "vertical")
+else:
+    print("Slice orientation= " + "Horizontal")
+
+# Open Input File
+im = Image.open(input_image)
+print(im.format, im.size, im.mode)
+
+# Get size
+width = im.size[0]
+height = im.size[1]
+print("Input Image Size -- Width = ", width, "Height = ", height)
+
+# Resize Syntax
+# Syntax: Image.resize(size, resample=0)
+# Parameters:
+# size – The requested size in pixels, as a 2-tuple: (width, height).
+# resample – An optional resampling filter. This can be one of PIL.Image.NEAREST (use nearest neighbour),
+# PIL.Image.BILINEAR (linear interpolation), PIL.Image.BICUBIC (cubic spline interpolation), or
+# PIL.Image.LANCZOS (a high-quality downsampling filter). If omitted, or if the image has mode “1” or “P”,
+# it is set PIL.Image.NEAREST.
+
+# Resize to max_pixel
+if width > height:
+    print(" -- Format Landscape")
+    ratio_reduction = width / max_pixel
+    new_size = (int(width / ratio_reduction), int(height / ratio_reduction))
+elif width < height:
+    print(" -- Format Portrait")
+    ratio_reduction = height / max_pixel
+    new_size = (int(width / ratio_reduction), int(height / ratio_reduction))
+else:
+    print(" -- Format Square")
+    ratio_reduction = width / max_pixel
+    new_size = (int(width / ratio_reduction), int(height / ratio_reduction))
+
+im = im.resize(new_size, resample=Image.Resampling.LANCZOS)
+
+# Get size
+width = im.size[0]
+height = im.size[1]
+print("New Image size = ", "Width = ", width, "Height = ", height)
+
+# im.show()
+
+# Crop Syntax
+# Syntax: PIL.Image.crop(box = None)
+# Parameters:
+# box – a 4-tuple defining the left, upper, right, and lower pixel coordinate.
+# Return type: Image (Returns a rectangular region as (left, upper, right, lower)-tuple).
+# Return: An Image object.
+
+# Setting the points for cropped image
+left = 0
+top = 0
+right = int(width / nb_slices) - 1
+bottom = height
+
+box = (left, top, right, bottom)
+
+# Cropped image of above dimension
+# (It will not change original image)
+# im1 = im.crop((left, top, right, bottom))
+# im1 = im.crop(box)
+# im1.show()
+
+# save crop image
+# im1.save(output_image, 'jpeg')
+
+# Loop for each slice
+r = range(nb_slices)
+for i in r:
+    left = i * int(width / nb_slices)
+    top = 0
+    right = (i + 1) * int(width / nb_slices) - 1
+    bottom = height
+
+    box = (left, top, right, bottom)
+    print(box)
+    im1 = im.crop(box)
+    im1.save(output_image + "_" + str(i) + ".jpg", 'jpeg')
+    print("-- Slice created : " + output_image + "_" + str(i) + ".jpg")
+
+print("End ...")
