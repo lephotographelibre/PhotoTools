887352ae934e9b4e0f0875f7a6a8ea4e301e60f0
lephotographelibre <lephotographelibre@yahoo.com>
2022-10-03 11:01:40 +0200

    Add command line parameters

--- slicer.py
+++ slicer.py
@@ -1,129 +1,152 @@
 import ntpath
+import argparse
+import sys
+import os
+import os.path
 from PIL import Image
+
 
 # TODO transform s=V/H slicer in crop box slicer (x * y)
 
-# Input Parameters
-max_pixel = 2000
-nb_slices = 5
-# TODO crop = "3x5"
-slice_orientation = "H"
-input_image = "images/vertical01.jpg"
+def main():
+    # Input Parameters
 
-# Prepare Data - Variables
-print("Input file = " + input_image)
-# extract filename from imput file name
-filename = ntpath.basename(input_image)
-index = filename.index('.')
-filename = filename[:index]
-# print(file_name)
-output_image = "out/" + filename + "_slice"
-print("Output file = " + output_image)
+    args = parse_parameters()
+    print('-- Input parameters args --', str(args))
 
-# Open Input File
-im = Image.open(input_image)
-print(im.format, im.size, im.mode)
+    if not os.path.isfile(args.inputimage):
+        raise FileNotFoundError(args.inputimage)
+        exit()
 
-# Get size
-width = im.size[0]
-height = im.size[1]
-print("Input Image Size -- Width = ", width, "Height = ", height)
+    # Prepare Data - Variables
+    print("Input file = " + args.inputimage)
+    # extract filename from imput file name
+    filename = ntpath.basename(args.inputimage)
+    index = filename.index('.')
+    filename = filename[:index]
+    # print(file_name)
+    output_image = "out/" + filename + "_slice"
+    print("Output file = " + output_image)
 
-# Resize Syntax
-# Syntax: Image.resize(size, resample=0)
-# Parameters:
-# size – The requested size in pixels, as a 2-tuple: (width, height).
-# resample – An optional resampling filter. This can be one of PIL.Image.NEAREST (use nearest neighbour),
-# PIL.Image.BILINEAR (linear interpolation), PIL.Image.BICUBIC (cubic spline interpolation), or
-# PIL.Image.LANCZOS (a high-quality downsampling filter). If omitted, or if the image has mode “1” or “P”,
-# it is set PIL.Image.NEAREST.
+    # Open Input File
+    im = Image.open(args.inputimage)
+    print(im.format, im.size, im.mode)
 
-# Resize to max_pixel
-if width > height:
-    print(" -- Format Landscape")
-    ratio_reduction = width / max_pixel
-    new_size = (int(width / ratio_reduction), int(height / ratio_reduction))
-elif width < height:
-    print(" -- Format Portrait")
-    ratio_reduction = height / max_pixel
-    new_size = (int(width / ratio_reduction), int(height / ratio_reduction))
-else:
-    print(" -- Format Square")
-    ratio_reduction = width / max_pixel
-    new_size = (int(width / ratio_reduction), int(height / ratio_reduction))
+    # Get size
+    width = im.size[0]
+    height = im.size[1]
+    print("Input Image Size -- Width = ", width, "Height = ", height)
 
-im = im.resize(new_size, resample=Image.Resampling.LANCZOS)
+    # Resize Syntax
+    # Syntax: Image.resize(size, resample=0)
+    # Parameters:
+    # size – The requested size in pixels, as a 2-tuple: (width, height).
+    # resample – An optional resampling filter. This can be one of PIL.Image.NEAREST (use nearest neighbour),
+    # PIL.Image.BILINEAR (linear interpolation), PIL.Image.BICUBIC (cubic spline interpolation), or
+    # PIL.Image.LANCZOS (a high-quality downsampling filter). If omitted, or if the image has mode “1” or “P”,
+    # it is set PIL.Image.NEAREST.
 
-# Get size
-width = im.size[0]
-height = im.size[1]
-print("New Image size = ", "Width = ", width, "Height = ", height)
+    # Resize to args.maxsize
+    if width > height:
+        print(" -- Format Landscape")
+        ratio_reduction = width / args.maxsize
+        new_size = (int(width / ratio_reduction), int(height / ratio_reduction))
+    elif width < height:
+        print(" -- Format Portrait")
+        ratio_reduction = height / args.maxsize
+        new_size = (int(width / ratio_reduction), int(height / ratio_reduction))
+    else:
+        print(" -- Format Square")
+        ratio_reduction = width / args.maxsize
+        new_size = (int(width / ratio_reduction), int(height / ratio_reduction))
 
-# im.show()
+    im = im.resize(new_size, resample=Image.Resampling.LANCZOS)
 
-# Crop Syntax
-# Syntax: PIL.Image.crop(box = None)
-# Parameters:
-# box – a 4-tuple defining the left, upper, right, and lower pixel coordinate.
-#      the first two numbers define the top-left coordinates of the outtake (x,y),
-#      while the last two define the right-bottom coordinates
-#
-# (top,left)
-#	+-----------------------+
-#	|                       |
-#	|                       |
-#	|                       |
-#	+-----------------------+
-#                                (right, lower)
-#
-# Return type: Image (Returns a rectangular region as (left, upper, right, lower)-tuple).
-# Return: An Image object.
+    # Get size
+    width = im.size[0]
+    height = im.size[1]
+    print("New Image size = ", "Width = ", width, "Height = ", height)
 
-# Setting the points for cropped image
-left = 0
-top = 0
-right = int(width / nb_slices) - 1
-bottom = height
+    # im.show()
 
-box = (left, top, right, bottom)
+    # Crop Syntax
+    # Syntax: PIL.Image.crop(box = None)
+    # Parameters:
+    # box – a 4-tuple defining the left, upper, right, and lower pixel coordinate.
+    #      the first two numbers define the top-left coordinates of the outtake (x,y),
+    #      while the last two define the right-bottom coordinates
+    #
+    # (top,left)
+    #	+-----------------------+
+    #	|                       |
+    #	|                       |
+    #	|                       |
+    #	+-----------------------+
+    #                                (right, lower)
+    #
+    # Return type: Image (Returns a rectangular region as (left, upper, right, lower)-tuple).
+    # Return: An Image object.
 
-# Cropped image of above dimension
-# (It will not change original image)
-# im1 = im.crop((left, top, right, bottom))
-# im1 = im.crop(box)
-# im1.show()
+    # Setting the points for cropped image
+    left = 0
+    top = 0
+    right = int(width / args.slicenumber) - 1
+    bottom = height
 
-# save crop image
-# im1.save(output_image, 'jpeg')
+    box = (left, top, right, bottom)
 
-# Loop for each slice (H or V)
+    # Cropped image of above dimension
+    # (It will not change original image)
+    # im1 = im.crop((left, top, right, bottom))
+    # im1 = im.crop(box)
+    # im1.show()
 
-if slice_orientation == "V":
-    print("Slice orientation = " + "vertical")
-    for i in range(nb_slices):
-        left = i * int(width / nb_slices)
-        top = 0
-        right = (i + 1) * int(width / nb_slices)
-        bottom = height
+    # save crop image
+    # im1.save(output_image, 'jpeg')
 
-        box = (left, top, right, bottom)
-        print(box)
-        im1 = im.crop(box)
-        im1.save(output_image + "_" + str(i) + ".jpg", 'jpeg')
-        print("-- Slice created : " + output_image + "_" + str(i) + ".jpg")
-else:
-    print("Slice orientation= " + "Horizontal")
-    for i in range(nb_slices):
-        left = 0
-        top = i * int(height / nb_slices)
-        right = width
-        bottom = (i + 1) * int(height / nb_slices)
+    # Loop for each slice (H or V)
 
-        box = (left, top, right, bottom)
-        print(box)
-        im1 = im.crop(box)
-        im1.save(output_image + "_" + "{:02d}".format(i) + ".jpg", 'jpeg')
-#        print("-- Slice created : " + output_image + "_" + str(i) + ".jpg")
-        print("-- Slice created : " + output_image + "_" + "{:02d}".format(i) + ".jpg")
+    if args.orientation == "V":
+        print("Slice orientation = " + "vertical")
+        for i in range(args.slicenumber):
+            left = i * int(width / args.slicenumber)
+            top = 0
+            right = (i + 1) * int(width / args.slicenumber)
+            bottom = height
 
-print("End ...")
+            box = (left, top, right, bottom)
+            print(box)
+            im1 = im.crop(box)
+            im1.save(output_image + "_" + "{:02d}".format(i) + ".jpg", 'jpeg')
+            print("-- Slice created : " + output_image + "_" + "{:02d}".format(i) + ".jpg")
+    else:
+        print("Slice orientation= " + "Horizontal")
+        for i in range(args.slicenumber):
+            left = 0
+            top = i * int(height / args.slicenumber)
+            right = width
+            bottom = (i + 1) * int(height / args.slicenumber)
+
+            box = (left, top, right, bottom)
+            print(box)
+            im1 = im.crop(box)
+            im1.save(output_image + "_" + "{:02d}".format(i) + ".jpg", 'jpeg')
+            print("-- Slice created : " + output_image + "_" + "{:02d}".format(i) + ".jpg")
+
+
+def parse_parameters():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('inputimage')
+    # parser.add_argument('outputimage')
+    # parser.add_argument('-l', '--log-level')
+    parser.add_argument('--jpeg-quality', type=int, default=50)
+    parser.add_argument('--maxsize', type=int, default=2000)
+    # TODO crop = "3x5"
+    parser.add_argument('--slicenumber', type=int, default=5)
+    parser.add_argument('--orientation')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    sys.exit(main())
