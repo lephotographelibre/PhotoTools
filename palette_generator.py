b6b6c8d5a22b53f70bdc68a659dfee68e5f3e0b9
lephotographelibre <lephotographelibre@yahoo.com>
2022-10-08 10:57:54 +0200

    Add Percentage to each cell

--- palette_generator.py
+++ palette_generator.py
@@ -2,6 +2,7 @@
 # Building a color palette generator in Python
 # From: https://kylermintah.medium.com/coding-a-color-palette-generator-in-python-inspired-by-procreate-5x-b10df37834ae
 
+import sys
 import math
 import PIL
 import extcolors  # From: https://github.com/CairX/extract-colors-py
@@ -10,8 +11,18 @@ import urllib.request
 import matplotlib.pyplot as plt
 import webcolors
 
+
 from PIL import Image, ImageDraw
+from PIL import ImageFont
 from matplotlib import gridspec
+
+
+def main():
+    # Execution cell
+    # We simply need to call our study_image() function and pass in a valid image URL in our final cell.
+    # image_url = 'https://tinyurl.com/unsplash-painted-flowers'
+    image_url = 'https://lephotographelibre.files.wordpress.com/2018/03/port_de_l_rochelle_1024_logose.jpg'
+    study_image(image_url)
 
 
 # Parent function
@@ -19,8 +30,10 @@ from matplotlib import gridspec
 # We will need to define each of the helper functions defined.
 def study_image(image_path):
     img = fetch_image(image_path)
-    colors = extract_colors(img)
-    color_palette = render_color_platte(colors)
+    # colors = extract_colors(img) # Standard rendering fonction
+    # color_palette = render_color_platte(colors) # Standard rendering fonction
+    colors, pixel_count = extract_colors_pixelcount(img)
+    color_palette = render_color_percent(colors, pixel_count)
     overlay_palette(img, color_palette)
 
 
@@ -49,19 +62,37 @@ def extract_colors(img):
     return colors
 
 
+def extract_colors_pixelcount(img):
+    # tolerance = 32
+    tolerance = 16
+    limit = 24  # max cell number
+    colors, pixel_count = extcolors.extract_from_image(img, tolerance, limit)
+    #    print(colors)
+    print_result(colors, pixel_count)
+    return colors, pixel_count
+
+
 # from the GIT source https://github.com/CairX/extract-colors-py/blob/master/extcolors/command.py
 def print_result(colors, pixel_count):
     print("Extracted colors:")
     color_count = sum([color[1] for color in colors])
     for color in colors:
-        rgb = str(color[0])
-        # print(webcolors.normalize_integer_triplet(color[0]))
+        rgb = str(color[0])  # color[0] is the rgb tuples
         print(get_colour_name(color[0]))
-        count = color[1]
+        count = color[1]  # color[1] is the number is the color[0] pixel number
         percentage = "{:.2f}".format((float(count) / float(color_count)) * 100.0)
         print(f"{rgb:15}:{percentage:>7}% ({count})")
 
     print(f"\nPixels in output: {color_count} of {pixel_count}")
+
+
+# Get Pixel count percentage for a current_color
+def get_color_percentage(colors, current_color):
+    color_count = sum([color[1] for color in colors])
+    count = current_color[1]
+    percentage = "{:.2f}".format((float(count) / float(color_count)) * 100.0)
+    # percentage = math.trunc(float(count) / float(color_count) * 100.0)
+    return percentage
 
 
 # Get approminate Color Name (CSS21)
@@ -77,6 +108,7 @@ def get_colour_name(rgb_triplet):
     return min_colours[min(min_colours.keys())]
 
 
+# the standard rendering function
 def render_color_platte(colors):
     size = 100
     columns = 6
@@ -88,6 +120,30 @@ def render_color_platte(colors):
         x = int((idx % columns) * size)
         y = int(math.floor(idx / columns) * size)
         canvas.rectangle([(x, y), (x + size - 1, y + size - 1)], fill=color[0])
+
+    return result
+
+
+# the modified rendering function with % text in the palette cells
+def render_color_percent(colors, pixel_count):
+    size = 100
+    columns = 6
+    # Palette Size
+    width = int(min(len(colors), columns) * size)
+    height = int((math.floor(len(colors) / columns) + 1) * size)
+    #
+    # font = ImageFont.load_default()
+    font = ImageFont.truetype("Arial.ttf", 24)  # locally store Arial.ttf font file
+    # Palette Image
+    result = Image.new("RGBA", (width, height), (0, 0, 0, 0))
+    canvas = ImageDraw.Draw(result)
+    for idx, color in enumerate(colors):
+        x = int((idx % columns) * size)
+        y = int(math.floor(idx / columns) * size)
+        canvas.rectangle([(x, y), (x + size - 1, y + size - 1)], fill=color[0])
+        # TODO Calculate and write % (If color is White - Font is Black, Inf color is Black Font is White)
+        print("% = " + str(get_color_percentage(colors, color)))
+        canvas.text((x + 10, y + 10), get_color_percentage(colors, color) + " %", (255, 255, 255), font=font)
     return result
 
 
@@ -115,23 +171,6 @@ def get_font_color(rgb):
     # Create a color Palette with Percentage
 
 
-def render_color_percent(colors, pixel_count):
-    size = 100
-    columns = 3
-    # Palette Size
-    width = int(min(len(colors), columns) * size)
-    height = int((math.floor(len(colors) / columns) + 1) * size)
-    # Palette Image
-    result = Image.new("RGBA", (width, height), (0, 0, 0, 0))
-    canvas = ImageDraw.Draw(result)
-    for idx, color in enumerate(colors):
-        x = int((idx % columns) * size)
-        y = int(math.floor(idx / columns) * size)
-        canvas.rectangle([(x, y), (x + size - 1, y + size - 1)], fill=color[0])
-        # TODO Calculate and write % (If color is White - Font is Black, Inf color is Black Font is White)
-    return result
-
-
 def overlay_palette(img, color_palette):
     nrow = 2
     ncol = 1
@@ -147,8 +186,5 @@ def overlay_palette(img, color_palette):
     plt.show(block=True)
 
 
-# Execution cell
-# We simply need to call our study_image() function and pass in a valid image URL in our final cell.
-# image_url = 'https://tinyurl.com/unsplash-painted-flowers'
-image_url = 'https://lephotographelibre.files.wordpress.com/2018/03/port_de_l_rochelle_1024_logose.jpg'
-study_image(image_url)
+if __name__ == "__main__":
+    sys.exit(main())
